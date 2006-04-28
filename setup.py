b696fd3a94962d73cbf1def03f1de8a301e9ddfc
mbaas <devnull@localhost>
2006-04-28 10:09:48 +0000

    Modified the setup script so that it can be used to create source distributions. Added MANIFEST.in.

--- setup.py
+++ setup.py
@@ -1,17 +1,63 @@
+#!/usr/bin/env python
 # Copyright 2004 Roman Yakovenko.
 # Distributed under the Boost Software License, Version 1.0. (See
 # accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
-import os
+import sys, os, os.path
 from distutils import sysconfig
 from distutils.core import setup
+from distutils.cmd import Command
 
-setup( name="pygccxml"
-       , description="GCC-XML generated file reader"
-       , author="Roman Yakovenko"
-       , packages=[ 'pygccxml'
-                    , 'pygccxml.declarations'
-                    , 'pygccxml.parser'
-                    , 'pygccxml.utils' ]
+def generate_doc():
+    """Generate the epydoc reference manual.
+    """
+    print "Generating epydoc files..."
+    options = [ '--output="%s"'%os.path.join('docs', 'apidocs'),
+                '--docformat=epytext',
+                '--url=http://www.language-binding.net',
+                '--name=pygccxml',
+#                '--verbose',
+                'pygccxml']
+    cmd_line = "epydoc " + ' '.join( options )
+    print cmd_line
+    os.system(cmd_line)
+    
+
+class doc_cmd(Command):
+    """This is a new distutils command 'doc' to build the epydoc manual.
+    """
+
+    description = 'build the API reference using epydoc'
+    user_options = [('no-doc', None, "don't run epydoc")]
+    boolean_options = ['no-doc']
+
+    def initialize_options (self):
+        self.no_doc = 0
+        
+    def finalize_options (self):
+        pass
+    
+    def run(self):
+        if self.no_doc:
+            return
+        generate_doc()
+
+
+# Generate the doc when a source distribution is created
+if sys.argv[-1]=="sdist":
+    generate_doc()
+
+
+setup( name = "pygccxml",
+       version = "0.7.2",
+       description = "GCC-XML generated file reader",
+       author = "Roman Yakovenko",
+       author_email = "roman.yakovenko@gmail.com",
+       url = 'http://pygccxml.sourceforge.net',
+       packages = [ 'pygccxml',
+                    'pygccxml.declarations',
+                    'pygccxml.parser',
+                    'pygccxml.utils' ],
+       cmdclass = {"doc" : doc_cmd}
 )
