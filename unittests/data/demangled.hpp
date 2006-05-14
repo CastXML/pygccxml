e187370cad3ccd0d1f24281fa94d03fe9cca037e
roman_yakovenko <devnull@localhost>
2006-05-14 07:27:38 +0000

    adding test case for demangled property

---
+++ unittests/data/demangled.hpp
@@ -0,0 +1,27 @@
+// Copyright 2004 Roman Yakovenko.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef __demangled_hpp
+#define __demangled_hpp
+
+namespace demangled{
+
+template< unsigned long i1, unsigned long i2, unsigned long i3>
+struct item_t{
+   static const unsigned long v1 = i1;
+   static const unsigned long v2 = i2;
+   static const unsigned long v3 = i3;
+};
+
+struct buggy{
+   typedef unsigned long ulong;
+   typedef item_t< ulong( 0xDEECE66DUL ) | (ulong(0x5) << 32), 0xB, ulong(1) << 31 > my_item_t;
+   my_item_t my_item_var;
+};
+
+
+}
+
+#endif//__demangled_hpp
