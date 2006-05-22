0b0996dfb63b64fca95fac77769913ce790e9784
roman_yakovenko <devnull@localhost>
2006-05-22 20:34:23 +0000

    improving documentation

--- docs/example/example.py
+++ docs/example/example.py
@@ -3,21 +3,24 @@
 # accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
+import sys
+sys.path.append('../..') #adding pygccxml to the path
+
 from pygccxml import parser
 from pygccxml import declarations
 
 #configure GCC-XML parser
-config = parser.config_t( gccxml_path=r'/home/roman/gccxml/bin/gccxml' )
+config = parser.config_t( gccxml_path='/home/roman/gccxml-build/bin/gccxml' )
+
 #parsing source file
-global_ns = parser.parse( ['core_class_hierarchy.hpp'], config )
+decls = parser.parse( ['core_class_hierarchy.hpp'], config )
+global_ns = declarations.get_global_namespace( decls )
+
 #printing all declarations found in file and its includes
 declarations.print_declarations( global_ns )
-#selecting all classes
-all_decls = declarations.make_flatten( global_ns )
-all_classes = filter( lambda decl: isinstance( decl, declarations.class_t )
-                      , all_decls )
+
 #print all base and derived class names
-for class_ in all_classes:
+for class_ in global_ns.classes():
     print class_.name
     print '\tbases: ', `[base.related_class.name for base in class_.bases]`
     print '\tderived: ', `[derive.related_class.name for derive in class_.derived]`
