0d8b8cdbd4ae125b1177a877505d7bb3c4b6a3b2
roman_yakovenko <devnull@localhost>
2006-10-19 06:52:42 +0000

    incrementing version numbers

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ if sys.argv[-1]=="sdist":
 
 
 setup( name = "pygccxml",
-       version = "0.8.2",
+       version = "0.8.3",
        description = "GCC-XML generated file reader",
        author = "Roman Yakovenko",
        author_email = "roman.yakovenko@gmail.com",
