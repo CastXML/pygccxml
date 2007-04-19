2ff509dfd423374dbb2eba32299d04e84ec725d0
roman_yakovenko <devnull@localhost>
2007-04-19 16:26:58 +0000

    testing performance on pretty big xml file ( 31Mb )

--- unittests/test_performance.py
+++ unittests/test_performance.py
@@ -6,19 +6,22 @@
 import os
 import sys
 import time
+import pstats
+import cProfile
 import autoconfig
-
 from pygccxml import *
+
+
 
 dcache_file_name = os.path.join( autoconfig.data_directory, 'pygccxml.cache' )
 if os.path.exists(dcache_file_name):
     os.remove(dcache_file_name)
 
 def test_on_windows_dot_h():
-    windows_header = r"C:\Program Files\Microsoft Visual Studio .NET 2003\Vc7\PlatformSDK\Include\windows.h"
+    windows_header = r"D:\Program Files\Microsoft Visual Studio .NET 2003\Vc7\PlatformSDK\Include\windows.h"
     clock_prev = time.clock()
     dcache = parser.file_cache_t(dcache_file_name)
-    reader = parser.source_reader_t( parser.config_t(), dcache )
+    reader = parser.source_reader_t( parser.config_t(gccxml_path=autoconfig.gccxml_path), dcache )
     reader.read_file(windows_header)
     dcache.flush()
     clock_now = time.clock()
@@ -26,7 +29,7 @@ def test_on_windows_dot_h():
 
     clock_prev = time.clock()
     dcache = parser.file_cache_t(dcache_file_name)    
-    reader = parser.source_reader_t( parser.config_t(), dcache )
+    reader = parser.source_reader_t( parser.config_t(gccxml_path=autoconfig.gccxml_path), dcache )
     reader.read_file(windows_header)
     clock_now = time.clock()
     print 'with cache   : %f seconds' % ( clock_now - clock_prev )
@@ -37,7 +40,7 @@ def test_source_on_include_std_dot_hpp():
     include_std_header = os.path.join( autoconfig.data_directory, 'include_std.hpp' )
     clock_prev = time.clock()
     dcache = parser.file_cache_t(dcache_file_name)
-    reader = parser.source_reader_t( parser.config_t(), dcache )
+    reader = parser.source_reader_t( parser.config_t(gccxml_path=autoconfig.gccxml_path), dcache )
     reader.read_file(include_std_header)
     dcache.flush()
     clock_now = time.clock()
@@ -45,7 +48,7 @@ def test_source_on_include_std_dot_hpp():
     
     clock_prev = time.clock()
     dcache = parser.file_cache_t(dcache_file_name)    
-    reader = parser.source_reader_t( parser.config_t(), dcache )
+    reader = parser.source_reader_t( parser.config_t(gccxml_path=autoconfig.gccxml_path), dcache )
     reader.read_file(include_std_header)
     clock_now = time.clock()
     print 'with cache   : %f seconds' % ( clock_now - clock_prev )
@@ -57,7 +60,7 @@ def test_project_on_include_std_dot_hpp():
     include_std_header = os.path.join( autoconfig.data_directory, 'include_std.hpp' )
     clock_prev = time.clock()
     dcache = parser.file_cache_t(dcache_file_name)
-    reader = parser.project_reader_t( parser.config_t(), dcache )
+    reader = parser.project_reader_t( parser.config_t(gccxml_path=autoconfig.gccxml_path), dcache )
     reader.read_files([include_std_header])
     dcache.flush()
     clock_now = time.clock()
@@ -65,29 +68,42 @@ def test_project_on_include_std_dot_hpp():
     
     clock_prev = time.clock()
     dcache = parser.file_cache_t(dcache_file_name)    
-    reader = parser.project_reader_t( parser.config_t(), dcache )
+    reader = parser.project_reader_t( parser.config_t(gccxml_path=autoconfig.gccxml_path), dcache )
     reader.read_files([include_std_header])
     clock_now = time.clock()
     print 'with cache   : %f seconds' % ( clock_now - clock_prev )
 
 def profile_project():
     include_std_header = os.path.join( autoconfig.data_directory, 'include_std.hpp' )
-    reader = parser.project_reader_t( parser.config_t() )
+    reader = parser.project_reader_t( parser.config_t(gccxml_path=autoconfig.gccxml_path) )
     reader.read_files([include_std_header])
 
 def profile_project2():
-    include_std_header = r"C:\Program Files\Microsoft Visual Studio .NET 2003\Vc7\PlatformSDK\Include\windows.h"
-    reader = parser.project_reader_t( parser.config_t() )
+    include_std_header = r"D:\Program Files\Microsoft Visual Studio .NET 2003\Vc7\PlatformSDK\Include\windows.h"
+    reader = parser.project_reader_t( parser.config_t(gccxml_path=autoconfig.gccxml_path) )
     reader.read_files([include_std_header])
 
+def parse_big_file():
+    reader = parser.project_reader_t( parser.config_t(gccxml_path=autoconfig.gccxml_path) )
+    
+    reader.read_files([parser.create_gccxml_fc( os.path.join( autoconfig.data_directory, 'big.xml' ) )])
+    
+
 if __name__ == "__main__":
-    test_on_windows_dot_h()
-    test_source_on_include_std_dot_hpp()
-    test_project_on_include_std_dot_hpp()
-    #~ import profile
-    #~ profile.run('profile_project()', 'pygccxml.profile')    
-    #~ import pstats
-    #~ pdata = pstats.Stats('pygccxml.profile')
-    #~ pdata.strip_dirs()
-    #~ pdata.sort_stats('cumulative', 'time').print_stats(15)
-    #~ pdata.print_callers('find_all_declarations')
+    
+    #~ test_on_windows_dot_h()
+    #~ test_source_on_include_std_dot_hpp()
+    #~ test_project_on_include_std_dot_hpp()
+    print 'running'
+    cProfile.run('parse_big_file()', 'pygccxml.profile')    
+    print 'running - done'
+    print 'loading file'
+    pdata = pstats.Stats('pygccxml.profile')
+    print 'loading file - done'
+    print 'striping dirs'
+    pdata.strip_dirs()
+    print 'striping dirs - done'
+    print 'sorting stats'
+    pdata.sort_stats('cumulative', 'time').print_stats(476)
+    print 'sorting stats - done'
+    #pdata.print_callers('find_all_declarations')
