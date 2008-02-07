c8616a5121ae8abc6aa344aa60b1bfc5e773c9d4
roman_yakovenko <devnull@localhost>
2008-02-07 07:47:26 +0000

    removing synopsis parser

--- pygccxml/parser/pdb_reader.py
+++ pygccxml/parser/pdb_reader.py
@@ -1,7 +1,11 @@
 import os
 import sys
+import comtypes
 import comtypes.client
 from sets import Set as set
+
+comtypes.client.gen_dir = r'D:\dev\language-binding\sources\pygccxml_dev\pygccxml\parser\gen'
+print comtypes.client.GetModule( r'D:\Program Files\Microsoft Visual Studio .NET 2003\Visual Studio SDKs\DIA SDK\bin\msdia71.dll' )
 
 #~ MODULE_IDENTIFIER = ('{106173A0-0173-4e5c-84E7-E915422BE997}', 0, 2, 0)
 #~ MODULE_PATH = r'Lib\site-packages\win32com\gen_py\106173A0-0173-4e5c-84E7-E915422BE997x0x2x0.py'
@@ -17,8 +21,9 @@ from sets import Set as set
 
 #~ msdia = win32com.client.gencache.EnsureModule( *MODULE_IDENTIFIER )
 
-ds = comtypes.client.CreateObject( "{e60afbee-502d-46ae-858f-8272a09bd707}" )
-ds.loadDataFromPdb( 'xxx.pdb' )
+#ds = comtypes.client.CreateObject( "{e60afbee-502d-46ae-858f-8272a09bd707}" )
+#print dir( ds )
+#ds.loadDataFromPdb( 'xxx.pdb' )
 
 #~ ds = msdia.DiaSource()
 #~ ds.loadDataFromPdb( 'xxx.pdb' )
