2b76d08f421c68faabafc5418ce5c85f7d689782
Michka Popoff <michkapopoff@gmail.com>
2014-05-27 21:46:38 +0200

    Pep8 cleanup

--- pygccxml/declarations/compilers.py
+++ pygccxml/declarations/compilers.py
@@ -11,11 +11,13 @@ GCC_XML_06 = "GCC-XML 0.6"
 GCC_XML_07 = "GCC-XML 0.7"
 GCC_XML_09 = "GCC-XML 0.9"
 GCC_XML_09_BUGGY = "GCC-XML 0.9 BUGGY"
-#revision 122:
-#After this fix, all constructors and destructors that exist for a class
-#are dumped whether the user declared them or not.  Those that were
-#implicitly declared by the compiler are marked as "artificial".
+# revision 122:
+# After this fix, all constructors and destructors that exist for a class
+# are dumped whether the user declared them or not.  Those that were
+# implicitly declared by the compiler are marked as "artificial".
 
-def on_missing_functionality( compiler, functionality ):
-    raise  NotImplementedError( '"%s" compiler doesn\'t support functionality "%s"'
-                                % ( compiler, functionality ))
+
+def on_missing_functionality(compiler, functionality):
+    raise NotImplementedError(
+        '"%s" compiler doesn\'t support functionality "%s"' %
+        (compiler, functionality))
