19fdf8ae1add81c4629cd3e41d7d2c5bfcb2dd46
Michka Popoff <michkapopoff@gmail.com>
2014-05-31 11:28:24 +0200

    Add Pep8 tests for the examples and setup files

--- unittests/pep8_tester.py
+++ unittests/pep8_tester.py
@@ -10,7 +10,7 @@ class tester_t(unittest.TestCase):
         unittest.TestCase.__init__(self, *args)
 
     def test_pep8_conformance_unitests(self):
-        """Pep8 conformance test
+        """Pep8 conformance test (unitests)
 
         Runs on the unittest directory.
         """
@@ -20,16 +20,10 @@ class tester_t(unittest.TestCase):
         # Get the path to current directory
         path = os.path.dirname(os.path.realpath(__file__))
 
-        pep8style = pep8.StyleGuide()
-        result = pep8style.check_files(paths=[path])
-
-        if result.total_errors != 0:
-            self.assertEqual(
-                result.total_errors, 0,
-                "Found code style errors (and warnings).")
+        self.run_check(path)
 
     def test_pep8_conformance_pygccxml(self):
-        """Pep8 conformance test
+        """Pep8 conformance test (pygccxml)
 
         Runs on the pygccxml directory.
         """
@@ -39,6 +33,39 @@ class tester_t(unittest.TestCase):
         # Get the path to current directory
         path = os.path.dirname(os.path.realpath(__file__))
         path = path + "/../pygccxml/"
+
+        self.run_check(path)
+
+    def test_pep8_conformance_example(self):
+        """Pep8 conformance test (examples)
+
+        Runs on the example file in the docs.
+        """
+
+        print("\r\n")
+
+        # Get the path to current directory
+        path = os.path.dirname(os.path.realpath(__file__))
+        path = path + "/../docs/example/"
+
+        self.run_check(path)
+
+    def test_pep8_conformance_setup(self):
+        """Pep8 conformance test (setup)
+
+        Runs on the setup.py file
+        """
+
+        print("\r\n")
+
+        # Get the path to current directory
+        path = os.path.dirname(os.path.realpath(__file__))
+        path = path + "/../setup.py"
+
+        self.run_check(path)
+
+    def run_check(self, path):
+        """Common method to run the pep8 test."""
 
         pep8style = pep8.StyleGuide()
         result = pep8style.check_files(paths=[path])
