5ac471bea9ccd780ac4a93732ee5897cd0bf2c29
Michka Popoff <michkapopoff@gmail.com>
2014-06-09 16:15:58 +0200

    Update licence to Apache 2 and add NOTICE file

--- unittests/data/covariant_returns.hpp
+++ unittests/data/covariant_returns.hpp
@@ -1,7 +1,24 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
 // Copyright 2004-2008 Roman Yakovenko.
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0.
+// See http://www.boost.org/LICENSE_1_0.txt
 
 #ifndef __bug_virtual_functions_overload_to_be_exported_hpp__
 #define __bug_virtual_functions_overload_to_be_exported_hpp__
@@ -15,7 +32,7 @@ struct more_data_t : public data_t{
 
 struct algorithm_t{
     algorithm_t(){};
-    virtual data_t* f(){ 
+    virtual data_t* f(){
         data_t* d = new data_t();
         d->i = 0;
         return d;
@@ -25,12 +42,12 @@ struct algorithm_t{
 class better_algorithm_t : public algorithm_t{
 public:
     better_algorithm_t(){};
-    virtual more_data_t* f(){ 
+    virtual more_data_t* f(){
         more_data_t* d = new more_data_t();
         d->i = 1;
         return d;
     }
 
 };
- 
+
 #endif//__bug_virtual_functions_overload_to_be_exported_hpp__
--- unittests/data/string_traits.hpp
+++ unittests/data/string_traits.hpp
@@ -1,12 +1,29 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
 // Copyright 2004-2008 Roman Yakovenko.
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0.
+// See http://www.boost.org/LICENSE_1_0.txt
 
 #include <string>
 
 namespace string_traits{
-namespace yes{    
+namespace yes{
     typedef std::string x1;
     typedef const std::string x2;
 }
@@ -15,11 +32,11 @@ namespace no{
     typedef int x1;
     typedef std::string& x2;
 }
-    
+
 }
 
 namespace wstring_traits{
-namespace yes{    
+namespace yes{
     typedef std::wstring x1;
     typedef const std::wstring x2;
 }
@@ -28,5 +45,5 @@ namespace no{
     typedef int x1;
     typedef std::wstring& x2;
 }
-    
+
 }
