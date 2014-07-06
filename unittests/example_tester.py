aeac86580de5083e925aeed1d1420856ba872ed5
Michka Popoff <michkapopoff@gmail.com>
2014-07-06 12:09:55 +0200

    Add .coveragerc file Fix coverage for examples.py file

--- unittests/example_tester.py
+++ unittests/example_tester.py
@@ -12,10 +12,16 @@ class tester_t(unittest.TestCase):
     def test_example(self):
         """Runs the example in the docs directory"""
 
+        env = os.environ.copy()
+
         # Get the path to current directory
         path = os.path.dirname(os.path.realpath(__file__))
+        # Set the COVERAGE_PROCESS_START env. variable.
+        # Allows to cover files run in a subprocess
+        # http://nedbatchelder.com/code/coverage/subprocess.html
+        env["COVERAGE_PROCESS_START"] = path + "/../.coveragerc"
         path += "/../docs/example/example.py"
-        return_code = subprocess.call(["python", path])
+        return_code = subprocess.call(["python", path], env=env)
         self.assertFalse(
             return_code,
             msg="The example did not run correctly")
