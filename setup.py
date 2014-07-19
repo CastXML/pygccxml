8890fb38aeea20c3dfabc62992100f1d776ad34f
Michka Popoff <michkapopoff@gmail.com>
2014-07-19 15:43:04 +0200

    Use setup tools instead of distutils for the setup.py script This is the new recommended way to distribute packages, see: https://python-packaging-user-guide.readthedocs.org/en/latest/current.html
    Remove the pkg-info file, this file is created by setup tools when building the project.
    Some infos are not correctly exported to the PKG-Info file (the metadata version is still 1.1).
    This is a known bug: https://bitbucket.org/pypa/setuptools/issue/219/emit-metadata-12-data

--- setup.py
+++ setup.py
@@ -4,16 +4,33 @@
 # Distributed under the Boost Software License, Version 1.0.
 # See http://www.boost.org/LICENSE_1_0.txt
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="pygccxml",
       version="v1.6.0",
-      description="GCC-XML generated file reader",
-      author="Insight Software Consortium.",
-      author_email="gccxml@gccxml.org",
-      packages=['pygccxml',
-                'pygccxml.declarations',
-                'pygccxml.parser',
-                'pygccxml.binary_parsers',
-                'pygccxml.utils']
+      author="Roman Yakovenko",
+      author_email="roman yakovenko at gmail com",
+      maintainer="Michka Popoff and the Insight Software Consortium",
+      maintainer_email="gccxml@gccxml.org",
+      description="Python package for easy C++ declarations navigation.",
+      url="https://github.com/gccxml/pygccxml",
+      download_url="https://github.com/gccxml/pygccxml/archive/master.zip",
+      license="Boost",
+      keywords="C++, declaration parser, gccxml",
+      install_requires=["Python >= 2.7"],
+      packages=["pygccxml",
+                "pygccxml.declarations",
+                "pygccxml.parser",
+                "pygccxml.binary_parsers",
+                "pygccxml.utils"],
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: POSIX",
+          "Programming Language :: Python",
+          "Topic :: Software Development",
+          ],
       )
