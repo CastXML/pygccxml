a2b12308425c6f784c379ad50194d33a2bc5aa86
Michka Popoff <michkapopoff@gmail.com>
2015-06-29 21:12:30 +0200

    Replace the gccxml10185 test for castxml  and explain why

--- unittests/gccxml10185_tester.py
+++ unittests/gccxml10185_tester.py
@@ -23,11 +23,30 @@ class tester_t(parser_test_case.parser_test_case_t):
         parser_test_case.parser_test_case_t.__init__(self, *args)
 
     def test(self):
+        """
+        The purpose of this test was to check if changes to GCCXML
+        would lead to changes in the outputted xml file (Meaning
+        the bug was fixed).
+
+        GCCXML wrongly outputted partial template specialization.
+        CastXML does not have this bug. In this case we check if
+        the template specialization can not be found; which is the
+        expected/wanted behaviour.
+
+        https://github.com/CastXML/CastXML/issues/20
+
+        """
+
         src_reader = parser.source_reader_t(self.config)
         global_ns = declarations.get_global_namespace(
             src_reader.read_string(code))
-        a = global_ns.class_('A<const char [N]>')
-        a.mem_fun('size')
+        if 'GCCXML' in global_ns.compiler:
+            a = global_ns.class_('A<const char [N]>')
+            a.mem_fun('size')
+        elif 'CastXML' in global_ns.compiler:
+            self.assertRaises(
+                global_ns.declaration_not_found_t,
+                lambda: global_ns.class_('A<const char [N]>'))
 
 
 def create_suite():
