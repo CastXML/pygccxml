4cf76121460ff4d3850ee932f2b682749a186702
Michka Popoff <michkapopoff@gmail.com>
2015-12-01 23:51:26 +0100

    All the examples need to be run

--- unittests/example_tester.py
+++ unittests/example_tester.py
@@ -1,5 +1,6 @@
 
 import os
+import fnmatch
 import unittest
 import subprocess
 
@@ -20,11 +21,19 @@ class tester_t(unittest.TestCase):
         # Allows to cover files run in a subprocess
         # http://nedbatchelder.com/code/coverage/subprocess.html
         env["COVERAGE_PROCESS_START"] = path + "/../.coveragerc"
-        path += "/../docs/example/example.py"
-        return_code = subprocess.call(["python", path], env=env)
-        self.assertFalse(
-            return_code,
-            msg="The example did not run correctly")
+        path += "/../docs/examples"
+
+        # Find all the examples files
+        file_paths = []
+        for root, dirnames, filenames in os.walk(path):
+            for file_path in fnmatch.filter(filenames, '*.py'):
+                file_paths.append(os.path.join(root, file_path))
+
+        for file_path in file_paths:
+            return_code = subprocess.call(["python", file_path], env=env)
+            self.assertFalse(
+                return_code,
+                msg="The example %s did not run correctly" % file_path)
 
 
 def create_suite():
