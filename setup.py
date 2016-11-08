3354fe1ded21ef083f0edca15c5b00a1904d4d79
Michka Popoff <michkapopoff@gmail.com>
2016-11-08 21:27:29 +0100

    Bump version number to 1.8.1

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@
 from setuptools import setup
 
 setup(name="pygccxml",
-      version="1.8.0",
+      version="1.8.1",
       author="Roman Yakovenko",
       author_email="roman yakovenko at gmail com",
       maintainer="Michka Popoff and the Insight Software Consortium",
       maintainer_email="castxml@public.kitware.com",
       description="Python package for easy C++ declarations navigation.",
       url="https://github.com/gccxml/pygccxml",
-      download_url="https://github.com/gccxml/pygccxml/archive/v1.8.0.tar.gz",
+      download_url="https://github.com/gccxml/pygccxml/archive/v1.8.1.tar.gz",
       license="Boost",
       keywords="C++, declaration parser, CastXML, gccxml",
       packages=["pygccxml",
