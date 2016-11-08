6d67c51ce7d6ab1278e42f255d44c5dfa69f1cfa
Michka Popoff <michkapopoff@gmail.com>
2016-11-08 22:22:35 +0100

    Test directory cache with and without compression

--- unittests/test_directory_cache.py
+++ unittests/test_directory_cache.py
@@ -3,6 +3,7 @@
 # Distributed under the Boost Software License, Version 1.0.
 # See http://www.boost.org/LICENSE_1_0.txt
 
+import shutil
 import unittest
 import parser_test_case
 
@@ -14,6 +15,7 @@ class Test(parser_test_case.parser_test_case_t):
     def __init__(self, *args):
         parser_test_case.parser_test_case_t.__init__(self, *args)
         self.header = "core_cache.hpp"
+        self.cache_dir = "unittests/data/directory_cache_test"
 
     def test_elaborated_types(self):
         """
@@ -21,8 +23,22 @@ class Test(parser_test_case.parser_test_case_t):
 
         """
 
+        # Clear the cache tree
+        shutil.rmtree(self.cache_dir)
+
+        # Test with compression OFF
+        cache = parser.directory_cache_t(directory=self.cache_dir)
+        # Generate a cache on first read
+        parser.parse([self.header], self.config, cache=cache)
+        # Read from the cache the second time
+        parser.parse([self.header], self.config, cache=cache)
+
+        # Clear the cache tree
+        shutil.rmtree(self.cache_dir)
+
+        # Test with compression ON
         cache = parser.directory_cache_t(
-            directory="unittests/data/directory_cache_test")
+            directory=self.cache_dir, compression=True)
         # Generate a cache on first read
         parser.parse([self.header], self.config, cache=cache)
         # Read from the cache the second time
