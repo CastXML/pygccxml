fc4c0f05d10b66fca29f02d67e1c2efe614ea085
Michka Popoff <michkapopoff@gmail.com>
2017-01-04 18:40:24 +0100

    Add missing licence header

--- unittests/pep8_tester.py
+++ unittests/pep8_tester.py
@@ -1,3 +1,7 @@
+# Copyright 2014-2016 Insight Software Consortium.
+# Copyright 2004-2008 Roman Yakovenko.
+# Distributed under the Boost Software License, Version 1.0.
+# See http://www.boost.org/LICENSE_1_0.txt
 
 import os
 import pep8
