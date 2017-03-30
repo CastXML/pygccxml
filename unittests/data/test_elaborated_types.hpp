66faf89c1011aa55e6628df93808f50026503aae
Michka Popoff <michkapopoff@gmail.com>
2017-03-30 23:34:51 +0200

    Give names that can be read

--- unittests/data/test_elaborated_types.hpp
+++ unittests/data/test_elaborated_types.hpp
@@ -5,147 +5,147 @@
 
 namespace elaborated_t {
 
-    class Foo1 {};
-    struct Foo2 {};
-    enum Foo3 {e1};
+    class FooClass {};
+    struct FooStruct {};
+    enum FooEnum {e1};
 
     namespace yes {
 
-        class Foo1 c1 = {};
-        const class Foo1 c2 = {};
-        volatile class Foo1 c3 = {};
-        volatile const class Foo1 c4 = {};
-        const volatile class Foo1 c5 = {};
+        class FooClass c1 = {};
+        const class FooClass c2 = {};
+        volatile class FooClass c3 = {};
+        volatile const class FooClass c4 = {};
+        const volatile class FooClass c5 = {};
 
-        typedef class Foo1 cc1;
-        typedef const class Foo1 cc2;
-        typedef volatile class Foo1 cc3;
-        typedef volatile const class Foo1 cc4;
-        typedef const volatile class Foo1 cc5;
+        typedef class FooClass cc1;
+        typedef const class FooClass cc2;
+        typedef volatile class FooClass cc3;
+        typedef volatile const class FooClass cc4;
+        typedef const volatile class FooClass cc5;
 
-        class Foo1 *c1ptr;
-        const class Foo1 *c2ptr;
-        volatile class Foo1 *c3ptr;
-        volatile const class Foo1 *c4ptr;
-        const volatile class Foo1 *c5ptr;
+        class FooClass *c1ptr;
+        const class FooClass *c2ptr;
+        volatile class FooClass *c3ptr;
+        volatile const class FooClass *c4ptr;
+        const volatile class FooClass *c5ptr;
 
-        typedef class Foo1 *cc1ptr;
-        typedef const class Foo1 *cc2ptr;
-        typedef volatile class Foo1 *cc3ptr;
-        typedef volatile const class Foo1 *cc4ptr;
-        typedef const volatile class Foo1 *cc5ptr;
+        typedef class FooClass *cc1ptr;
+        typedef const class FooClass *cc2ptr;
+        typedef volatile class FooClass *cc3ptr;
+        typedef volatile const class FooClass *cc4ptr;
+        typedef const volatile class FooClass *cc5ptr;
 
-        struct Foo2 s1 = {};
-        const struct Foo2 s2 = {};
-        volatile struct Foo2 s3 = {};
-        volatile const struct Foo2 s4 = {};
-        const volatile struct Foo2 s5 = {};
+        struct FooStruct s1 = {};
+        const struct FooStruct s2 = {};
+        volatile struct FooStruct s3 = {};
+        volatile const struct FooStruct s4 = {};
+        const volatile struct FooStruct s5 = {};
 
-        typedef struct Foo2 ss1;
-        typedef const struct Foo2 ss2;
-        typedef volatile struct Foo2 ss3;
-        typedef volatile const struct Foo2 ss4;
-        typedef const volatile struct Foo2 ss5;
+        typedef struct FooStruct ss1;
+        typedef const struct FooStruct ss2;
+        typedef volatile struct FooStruct ss3;
+        typedef volatile const struct FooStruct ss4;
+        typedef const volatile struct FooStruct ss5;
 
-        struct Foo2 *s1ptr;
-        const struct Foo2 *s2ptr;
-        volatile struct Foo2 *s3ptr;
-        volatile const struct Foo2 *s4ptr;
-        const volatile struct Foo2 *s5ptr;
+        struct FooStruct *s1ptr;
+        const struct FooStruct *s2ptr;
+        volatile struct FooStruct *s3ptr;
+        volatile const struct FooStruct *s4ptr;
+        const volatile struct FooStruct *s5ptr;
 
-        typedef struct Foo2 *ss1ptr;
-        typedef const struct Foo2 *ss2ptr;
-        typedef volatile struct Foo2 *ss3ptr;
-        typedef volatile const struct Foo2 *ss4ptr;
-        typedef const volatile struct Foo2 *ss5ptr;
+        typedef struct FooStruct *ss1ptr;
+        typedef const struct FooStruct *ss2ptr;
+        typedef volatile struct FooStruct *ss3ptr;
+        typedef volatile const struct FooStruct *ss4ptr;
+        typedef const volatile struct FooStruct *ss5ptr;
 
-        enum Foo3 e1;
-        //const enum Foo3 e2; (not valid c++)
-        volatile enum Foo3 e3;
-        //volatile const enum Foo3 e4; (not valid c++)
-        //const volatile enum Foo3 e5; (not valid c++)
+        enum FooEnum e1;
+        //const enum FooEnum e2; (not valid c++)
+        volatile enum FooEnum e3;
+        //volatile const enum FooEnum e4; (not valid c++)
+        //const volatile enum FooEnum e5; (not valid c++)
 
-        enum Foo3 *e1ptr;
-        const enum Foo3 *e2ptr;
-        volatile enum Foo3 *e3ptr;
-        volatile const enum Foo3 *e4ptr;
-        const volatile enum Foo3 *e5ptr;
+        enum FooEnum *e1ptr;
+        const enum FooEnum *e2ptr;
+        volatile enum FooEnum *e3ptr;
+        volatile const enum FooEnum *e4ptr;
+        const volatile enum FooEnum *e5ptr;
 
-        typedef enum Foo3 *ee1ptr;
-        typedef const enum Foo3 *ee2ptr;
-        typedef volatile enum Foo3 *ee3ptr;
-        typedef volatile const enum Foo3 *ee4ptr;
-        typedef const volatile enum Foo3 *ee5ptr;
+        typedef enum FooEnum *ee1ptr;
+        typedef const enum FooEnum *ee2ptr;
+        typedef volatile enum FooEnum *ee3ptr;
+        typedef volatile const enum FooEnum *ee4ptr;
+        typedef const volatile enum FooEnum *ee5ptr;
 
     }
 
     namespace no {
 
-        Foo1 c1 = {};
-        const Foo1 c2 = {};
-        volatile Foo1 c3 = {};
-        volatile const Foo1 c4 = {};
-        const volatile Foo1 c5 = {};
+        FooClass c1 = {};
+        const FooClass c2 = {};
+        volatile FooClass c3 = {};
+        volatile const FooClass c4 = {};
+        const volatile FooClass c5 = {};
 
-        typedef Foo1 cc1;
-        typedef const Foo1 cc2;
-        typedef volatile Foo1 cc3;
-        typedef volatile const Foo1 cc4;
-        typedef const volatile Foo1 cc5;
+        typedef FooClass cc1;
+        typedef const FooClass cc2;
+        typedef volatile FooClass cc3;
+        typedef volatile const FooClass cc4;
+        typedef const volatile FooClass cc5;
 
-        Foo1 *c1ptr;
-        const Foo1 *c2ptr;
-        volatile Foo1 *c3ptr;
-        volatile const Foo1 *c4ptr;
-        const volatile Foo1 *c5ptr;
+        FooClass *c1ptr;
+        const FooClass *c2ptr;
+        volatile FooClass *c3ptr;
+        volatile const FooClass *c4ptr;
+        const volatile FooClass *c5ptr;
 
-        typedef Foo1 *cc1ptr;
-        typedef const Foo1 *cc2ptr;
-        typedef volatile Foo1 *cc3ptr;
-        typedef volatile const Foo1 *cc4ptr;
-        typedef const volatile Foo1 *cc5ptr;
+        typedef FooClass *cc1ptr;
+        typedef const FooClass *cc2ptr;
+        typedef volatile FooClass *cc3ptr;
+        typedef volatile const FooClass *cc4ptr;
+        typedef const volatile FooClass *cc5ptr;
 
-        Foo2 s1 = {};
-        const Foo2 s2 = {};
-        volatile Foo2 s3 = {};
-        volatile const Foo2 s4 = {};
-        const volatile Foo2 s5 = {};
+        FooStruct s1 = {};
+        const FooStruct s2 = {};
+        volatile FooStruct s3 = {};
+        volatile const FooStruct s4 = {};
+        const volatile FooStruct s5 = {};
 
-        typedef Foo2 ss1;
-        typedef const Foo2 ss2;
-        typedef volatile Foo2 ss3;
-        typedef volatile const Foo2 ss4;
-        typedef const volatile Foo2 ss5;
+        typedef FooStruct ss1;
+        typedef const FooStruct ss2;
+        typedef volatile FooStruct ss3;
+        typedef volatile const FooStruct ss4;
+        typedef const volatile FooStruct ss5;
 
-        Foo2 *s1ptr;
-        const Foo2 *s2ptr;
-        volatile Foo2 *s3ptr;
-        volatile const Foo2 *s4ptr;
-        const volatile Foo2 *s5ptr;
+        FooStruct *s1ptr;
+        const FooStruct *s2ptr;
+        volatile FooStruct *s3ptr;
+        volatile const FooStruct *s4ptr;
+        const volatile FooStruct *s5ptr;
 
-        typedef Foo2 *ss1ptr;
-        typedef const Foo2 *ss2ptr;
-        typedef volatile Foo2 *ss3ptr;
-        typedef volatile const Foo2 *ss4ptr;
-        typedef const volatile Foo2 *ss5ptr;
+        typedef FooStruct *ss1ptr;
+        typedef const FooStruct *ss2ptr;
+        typedef volatile FooStruct *ss3ptr;
+        typedef volatile const FooStruct *ss4ptr;
+        typedef const volatile FooStruct *ss5ptr;
 
-        Foo3 e1;
-        //const enum Foo3 e2; (not valid c++)
-        volatile Foo3 e3;
-        //volatile const Foo3 e4; (not valid c++)
-        //const volatile Foo3 e5; (not valid c++)
+        FooEnum e1;
+        //const enum FooEnum e2; (not valid c++)
+        volatile FooEnum e3;
+        //volatile const FooEnum e4; (not valid c++)
+        //const volatile FooEnum e5; (not valid c++)
 
-        Foo3 *e1ptr;
-        const Foo3 *e2ptr;
-        volatile Foo3 *e3ptr;
-        volatile const Foo3 *e4ptr;
-        const volatile Foo3 *e5ptr;
+        FooEnum *e1ptr;
+        const FooEnum *e2ptr;
+        volatile FooEnum *e3ptr;
+        volatile const FooEnum *e4ptr;
+        const volatile FooEnum *e5ptr;
 
-        typedef Foo3 *ee1ptr;
-        typedef const Foo3 *ee2ptr;
-        typedef volatile Foo3 *ee3ptr;
-        typedef volatile const Foo3 *ee4ptr;
-        typedef const volatile Foo3 *ee5ptr;
+        typedef FooEnum *ee1ptr;
+        typedef const FooEnum *ee2ptr;
+        typedef volatile FooEnum *ee3ptr;
+        typedef volatile const FooEnum *ee4ptr;
+        typedef const volatile FooEnum *ee5ptr;
 
     }
 }
