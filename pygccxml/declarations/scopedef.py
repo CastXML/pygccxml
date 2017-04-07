a88dc59421744018cce86402b7ff5d829ab7deb8
Michka Popoff <michkapopoff@gmail.com>
2017-04-07 21:41:55 +0200

    Skip the elaborated_info class when initialising optimiser
    This is the same thing as the for the byte_info classe.
    Not really sure what this code is doing but this fixes multiple failing tests
    
    Traceback (most recent call last):
      File "unittests/find_container_traits_tester.py", line 26, in setUp
        Test.global_ns.init_optimizer()
      File "pygccxml/declarations/scopedef.py", line 312, in init_optimizer
        self._type2decls[type_].append(decl)
    KeyError: <class 'pygccxml.declarations.elaborated_info.elaborated_info'>

--- pygccxml/declarations/scopedef.py
+++ pygccxml/declarations/scopedef.py
@@ -13,6 +13,7 @@ from . import templates
 from . import declaration
 from . import mdecl_wrapper
 from . import byte_info
+from . import elaborated_info
 from . import runtime_errors
 from .. import utils
 
@@ -253,6 +254,8 @@ class scopedef_t(declaration.declaration_t):
             if base is declaration.declaration_t:
                 continue
             if base is byte_info.byte_info:
+                continue
+            if base is elaborated_info.elaborated_info:
                 continue
             if base in visited:
                 continue
