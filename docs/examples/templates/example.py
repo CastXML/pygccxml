e2142009106639ae14267889a25f1a09bd042828
Michka Popoff <michkapopoff@gmail.com>
2017-04-20 23:53:32 +0200

    Fix templates example
    The order of the declarations is not the same with castxml and gccxml,
    which makes the tests fail with gccxml.

--- docs/examples/templates/example.py
+++ docs/examples/templates/example.py
@@ -31,21 +31,30 @@ decls = parser.parse([filename], xml_generator_config)
 global_namespace = declarations.get_global_namespace(decls)
 ns = global_namespace.namespace("ns")
 
-print(ns.declarations[0])
+class_t_decl = []
+for d in ns.declarations:
+    if isinstance(d, declarations.class_declaration_t):
+        class_declaration_t = d
+    if isinstance(d, declarations.class_t):
+        class_t_decl.append(d)
+    if isinstance(d, declarations.free_function_t):
+        free_function_t_decl = d
+
+print(class_t_decl[0])
 # > ns::B [struct]
 
-print(ns.declarations[1])
+print(class_t_decl[1])
 # > ns::D [struct]
 
-print(ns.declarations[2])
+print(class_declaration_t)
 # > ns::T<ns::B::D, bool> [class declaration]
 
-print(ns.declarations[3])
-# > ns::T<ns::B::D, bool> ns::fun() [free function]
+print(free_function_t_decl)
+# > ns::T<ns::B::D, bool> ns::function() [free function]
 
-print(declarations.templates.is_instantiation(ns.declarations[2].name))
+print(declarations.templates.is_instantiation(class_declaration_t.name))
 # > True
 
-name, parameter_list = declarations.templates.split(ns.declarations[2].name)
+name, parameter_list = declarations.templates.split(class_declaration_t.name)
 print(name, parameter_list)
 # > 'T', ['ns::B::D', 'bool']
